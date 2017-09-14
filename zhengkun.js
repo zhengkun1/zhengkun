00f7c15a27f444e33e765903118fdea046969043
zhengkun1 <398786764@qq.com>
2017-09-14 14:02:50 +0800

    verify array's length

---
+++ zhengkun.js
@@ -0,0 +1,15 @@
+/*
+*   2017.9.14   zhengkun
+*/
+
+var arr =[1,2,3];
+console.log(arr.length);
+
+arr.length = 6
+console.log(arr);
+
+arr.length = 2
+console.log(arr);
+
+arr.length = 5
+console.log(arr);
