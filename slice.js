473391cd12446216c1ff50e67ad7626827b5a15e
zhengkun1 <398786764@qq.com>
2017-09-14 15:09:08 +0800

    slice

---
+++ slice.js
@@ -0,0 +1,16 @@
+/*
+*   2017-9-14     zhengkun  
+*/
+
+var arr = [1,2,3,4,5,'asd','fdd'];
+
+console.log(`arr.slice(1,3) => [${arr.slice(1,3)}]`);
+console.log(`arr.slice(1,4) => [${arr.slice(1,4)}]`);
+
+console.log(arr.slice(4));
+
+console.log(`arr.slice(4) =< ${arr.slice(5)}`)
+
+console.log(arr.slice(9));
+
+console.log(`arr.slice(1) =< [${arr.slice(2)}]`)
