d1ebc2efa1f65278f4d43b0b9da423cf0eba4937
zhengkun1 <398786764@qq.com>
2017-09-14 15:41:37 +0800

    array

--- array.js
+++ array.js
@@ -2,12 +2,16 @@
 *  2017.9.14   zhengkun 
 */
 
-var arr = [11,12,13,14]
+var arr = [11,12,13,14];
+
 arr.unshift(`a`,`b`);
 console.log(arr);
+
 arr.shift();
 console.log(arr);
+
 arr.shift();
 console.log(arr);
+
 arr.unshift(22.33);
 console.log(arr);
