f348e6311d1d4cef5b0cd187014aacdce84c4025
zhengkun1 <398786764@qq.com>
2017-09-15 20:59:03 +0800

    trainning

---
+++ arry1.js
@@ -0,0 +1,14 @@
+/**
+ * 2017-9-15 zhengkun
+ */
+
+var a = [1,2,3,4]
+
+a.length = 6 ;
+console.log(a);
+
+a.length = 2;
+console.log(a);
+
+a[5]='x';
+console.log(a)
---
+++ slice1.js
@@ -0,0 +1,6 @@
+/**
+ * 2017.9.15 zhengkun
+ */
+
+ var a = [1,2,3,4,5,6];
+  console.log(a.slice(0,3));
