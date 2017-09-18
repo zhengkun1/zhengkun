0500a5cddf94c944961697b67130b7deebeba044
zhengkun1 <398786764@qq.com>
2017-09-18 16:30:05 +0800

    9multipil9

--- 9x9.js
+++ 9x9.js
@@ -8,7 +8,6 @@ for (var b = 1; b <= 9;b++){
         a.push(`${b*c}`);
     }console.log(`${a}`);
 }
-*/
 
 
 for(var b = 1 ; b <= 9;b++){
@@ -16,4 +15,19 @@ for(var b = 1 ; b <= 9;b++){
     for(var c = 1;c <=b;c++){
         a =  a + `${b * c}\t`;
     };console.log(a)
+}
+
+*/
+
+
+
+console.log('\t1\t2\t3\t4\t5\t6\t7\t8\t9')
+
+for(var b = 1;b <= 9;b++ ){
+    var a = '';
+    for(var c = 1 ;c <= b;c++ ){
+        a  =  a + `${b*c}\t`;
+    }
+    a = `${b}\t` + a;
+    console.log(a); 
 }
