3ccf918fba506607a95d22787e95e23ebd631de4
zhengkun1 <398786764@qq.com>
2017-09-19 18:24:51 +0800

    person2

--- proson1.js
+++ proson1.js
@@ -16,17 +16,9 @@ zheng.getName = function(){
     return this.age
 };
 kun.setName = function(){
-    return this.age
+    return this.age+1
 };
-//console.log(zheng.setName());
-//console.log(kun.getName());
-console.log(zheng.getName);
-console.log(typeof zheng.getName);
-console.log(person.__proto__ ===   Function.prototype);
-console.log(zheng.getName.__proto__ === Function.prototype);
-console.log(kun.setName.__proto__ === Function.prototype);
-
-
 console.log(kun.setName());
 console.log(zheng.getName());
-
+console.log(zheng.getAge());
+console.log(kun.getAge());
