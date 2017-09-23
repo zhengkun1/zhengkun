a7c7f5a70005cccd27d6041dad2260bc20d09c62
zhengkun1 <398786764@qq.com>
2017-09-23 18:51:17 +0800

    promise

---
+++ proomise.js
@@ -0,0 +1,20 @@
+/**
+ * 2017-9-23  zhengkun
+ */
+
+function asyncFunction() {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve('Async Hello world');
+      }, 2000);
+    });
+  }
+  
+  asyncFunction().then(function (value) {
+    console.log(`2:${value}`);
+    return value;
+  }).then(function (value){
+      console.log(`3:${value}`)
+  }).catch(function(error){
+      console.log(`catch : ${error}`)
+  })
